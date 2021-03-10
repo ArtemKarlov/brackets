64dd34178e5d2de81aa81527f9ec2a91b97e371e
ArtemKarlov <ar.karlov@gmail.com>
2021-03-10 10:31:22 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) {
+    // console.log('odd number of brackets');
+    return false
+  };
+  
+  const stack = [];
+  const openBra = bracketsConfig.map(el => el[0]);
+  const closeBra = bracketsConfig.map(el => el[1]);
+  
+  if (!openBra.includes(str[0])) {
+    // console.log('closing bracket at first pos');
+    return false
+  };
+
+  for (const bracket of str) {    
+       
+    if (openBra.includes(bracket) && !stack.includes(bracket)) {
+      const index = openBra.indexOf(bracket);
+      stack.push(closeBra[index]);
+    } else {
+      const lastBracket = stack.pop();
+      if (lastBracket !== bracket) return false;
+    }
+  }
+  return (stack.length===0);
 }
